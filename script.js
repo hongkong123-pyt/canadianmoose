ce89dc4ac059855067f4de9a0346084ea6800b90
HongKong <djunavos@hotmail.com>
2025-09-28 14:31:58 +0200

    Change correct password from 'salmari' to 'vliegtuig'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // === Password Check ===
 function checkPassword() {
-  const correctPassword = "salmari";
+  const correctPassword = "vliegtuig";
   const input = document.getElementById("accessInput").value;
   const error = document.getElementById("errorMsg");
 
